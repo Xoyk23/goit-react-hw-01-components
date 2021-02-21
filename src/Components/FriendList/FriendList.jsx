b3c81417f3604db65a5d9ad476a81fdba28e8186
Ihor Prokopets <kiringa23@gmail.com>
2021-02-21 11:55:10 +0200

    add styles for 3rd task

--- src/Components/FriendList/FriendList.jsx
+++ src/Components/FriendList/FriendList.jsx
@@ -1,12 +1,14 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
 
-import FriendListItem from "./FriendListItem/FriendListItem";
+import FriendListItem from './FriendListItem/FriendListItem';
+
+import styles from './FriendList.module.css';
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className="friend-list">
-      {friends.map((friend) => (
+    <ul className={styles.list}>
+      {friends.map(friend => (
         <FriendListItem
           avatar={friend.avatar}
           name={friend.name}
--- src/Components/FriendList/FriendListItem/FriendListItem.jsx
+++ src/Components/FriendList/FriendListItem/FriendListItem.jsx
@@ -1,11 +1,15 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import styles from './FriendListItem.module.css';
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <li className="item">
-      <span>{isOnline}</span>
-      <img src={avatar} alt={name} width="50" />
+    <li className={styles.item}>
+      <span className={isOnline ? styles.online : styles.offline}>
+        {isOnline}
+      </span>
+      <img className={styles.avatar} src={avatar} alt={name} width="50" />
       <p>{name}</p>
     </li>
   );
